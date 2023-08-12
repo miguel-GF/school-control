1ea649cc11290a596cbbe0771e23fcb380e7c493
Miguel <miguel.gf.130@gmail.com>
2023-08-11 22:05:26 -0600

    [Utils] Se crear archivo notify para mensajes de notificacion

---
+++ resources/js/Utils/notify.js
@@ -0,0 +1,40 @@
+import { Notify } from 'quasar';
+
+export const notify = (message, tipo) => {
+  let color, icon;
+  let textColor = 'white';
+  switch (tipo) {
+    case "exito":
+      color = "positive";
+      icon  = "thumb_up";
+      break;
+    
+    case "advertencia":
+      color = "amber-5";
+      icon  = "priority_high";
+      textColor = "black";
+      break;
+    
+    case "error":
+      color = "negative";
+      icon  = "report_problem";
+      break;
+  
+    default:
+      color = "info";
+      icon  = "info";
+      break;
+  }
+
+  Notify.create({
+    progress: true,
+    message,
+    color,
+    icon,
+    position: 'top',
+    textColor,
+    actions: [
+      { label: 'x', color: textColor, handler: () => { /* ... */ } },
+    ],
+  });
+};
