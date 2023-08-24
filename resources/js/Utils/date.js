3dc91db0e5741aa17be13f1a938aae911baf6800
Miguel <miguel.gf.130@gmail.com>
2023-08-23 20:08:14 -0600

    [Date] Se agreag método para validar una fecha que sea válida de tipo 'YYYY/MM/DD'

--- resources/js/Utils/date.js
+++ resources/js/Utils/date.js
@@ -18,5 +18,38 @@ export const obtenerFechaActualOperacion = () => {
   let anio = fecha.getFullYear();
   let mes = ("0" + (fecha.getMonth() + 1)).slice(-2);
   let dia = ("0" + fecha.getDate()).slice(-2);
-  return anio + "-" + mes + "-" + dia;
+  return anio + "/" + mes + "/" + dia;
+};
+
+//Método para validar si es una fecha válida
+export const esFechaValida = (fechaParam) => {
+  const pattern = /^\d{4}\/\d{2}\/\d{2}$/;
+  if (!pattern.test(fechaParam)) {
+    return {
+      status: 300,
+      mensaje: 'El formato es incorrecto, debe ser YYYY/MM/DD',
+    };
+  }
+
+  const parts = fechaParam.split("/");
+  const year = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1; // Los meses en JavaScript son 0-indexados
+  const day = parseInt(parts[2], 10);
+  const date = new Date(year, month, day);
+
+  if (
+    date.getFullYear() !== year ||
+    date.getMonth() !== month ||
+    date.getDate() !== day
+  ) {
+    return {
+      status: 301,
+      mensaje: 'La fecha es inválida',
+    };
+  }
+
+  return {
+    status: 200,
+    mensaje: 'Fecha es correcta',
+  };
 };
