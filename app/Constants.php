09cc7dc428ebb1303f2aeb5370efa85e501bb395
Miguel <miguel.gf.130@gmail.com>
2024-06-10 20:29:47 -0600

    [Constantes] Se agregan constantes para tipo de archivos

--- app/Constants.php
+++ app/Constants.php
@@ -7,4 +7,11 @@ class Constants
   const ACTIVO_STATUS = "ACTIVO";
   const BAJA_STATUS = "BAJA";
   const ERROR_DATOS_INVALIDOS = 422;
+  const TIPO_ARCHIVO_CURRICULUM = 'cv';
+  const TIPO_ARCHIVO_INE = 'ine';
+  const TIPO_ARCHIVO_CURP = 'curp';
+  const TIPO_ARCHIVO_COMPROBANTE_DOMICILIO = 'domicilio';
+  const TIPO_ARCHIVO_COMPROBANTE_SITUACION_FISCAL = 'situacion_fiscal';
+  const TIPO_ARCHIVO_ACTA_NACIMIENTO = 'acta_nacimiento';
+  const TIPO_ARCHIVO_CEDULA_PROFESIONAL = 'cedula_profesional';
 }
