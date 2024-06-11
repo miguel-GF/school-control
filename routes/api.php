eac9dc1ca3f801f71117282a3dcd830656453647
Miguel <miguel.gf.130@gmail.com>
2024-06-10 20:53:34 -0600

    [Api] Se agrega endpoint para almacenar datos curriculum de docentes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+// use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DocenteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,13 +9,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
+Route::prefix('v1')->middleware('api.key')->group(function () {
+  // Route::prefix('auth')->group(function () {
+  //   Route::post('/login', [AuthController::class, 'loginMovil']);
+  // });
+  Route::prefix('docente')->group(function () {
+    Route::post('/cv', [DocenteController::class, 'guardarCV']);
+  });
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
